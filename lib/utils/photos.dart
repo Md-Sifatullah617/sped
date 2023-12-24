ee160631e55b83bdfc73e2504fc1d99d8f0de5e2
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2023-12-24 21:06:52 +0600

    assets added

--- lib/utils/photos.dart
+++ lib/utils/photos.dart
@@ -1,1 +1,15 @@
+//images
 String splashPhoto = "assets/images/Group 21.png";
+String banner1 = "assets/images/Banner.png";
+String food1 = "assets/images/food1.jpeg";
+String food2 = "assets/images/food2.jpeg";
+String food3 = "assets/images/food3.jpeg";
+String food4 = "assets/images/food4.jpeg";
+String food5 = "assets/images/food5.jpeg";
+
+//icons
+String googleIcons = "assets/icons/Group 6.png";
+String filterIcon = "assets/icons/Icon (1).png";
+String dropDownArrow = "assets/icons/Icon (2).png";
+String mapIcon = "assets/icons/Icon.png";
+String locationIndicator = "assets/icons/Vector.png";
