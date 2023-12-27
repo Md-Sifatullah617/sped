b80c8a4a19ac89d67b317397185c46c37f626ee4
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2023-12-28 00:15:24 +0600

    restaurant appbar done

--- lib/view/restaurants/restaurants.dart
+++ lib/view/restaurants/restaurants.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sped/utils/photos.dart';
 
 class RestaurantsScreen extends StatelessWidget {
   const RestaurantsScreen({super.key});
@@ -6,6 +8,54 @@ class RestaurantsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+          backgroundColor: AppColors.white,
+          elevation: 0,
+          leading: IconButton(
+            icon: Image.asset(
+              locationIndicator,
+            ),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(AppColors.btnColor2),
+            ),
+            onPressed: () {},
+          ),
+          title: Row(
+            children: [
+              RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: "Home",
+                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                            fontWeight: FontWeight.bold,
+                          ),
+                    ),
+                    TextSpan(
+                      text: ", Jl. Soekarno Hatta 15A",
+                      style: Theme.of(context).textTheme.titleSmall,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: Get.width * 0.02,
+              ),
+              Image.asset(
+                dropDownArrow,
+              )
+            ],
+          ),
+          actions: [
+            IconButton(
+              icon: Image.asset(
+                notification,
+              ),
+              
+              onPressed: () {},
+            ),
+          ],
+        ),
       body: Center(
         child: Text("Restaurants Screen"),
       ),
