356966ae221e2bbc0906e60e998b88427bc6dfba
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2023-12-30 03:39:28 +0600

    search page done

---
+++ lib/view/search/controller/search_controller.dart
@@ -0,0 +1,71 @@
+import 'package:get/get.dart';
+import 'package:sped/utils/photos.dart';
+
+class SearchesController extends GetxController {
+  List searchList = [
+    {
+      "image": food1,
+      "title": "Greasy Spoon Tjarhovsgatan",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "cost": "€3.50",
+      "time": "15-20 min",
+      "rating": "4.5",
+    },
+    {
+      "image": food2,
+      "title": "Expresso House Gotgatan",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "cost": "€2.50",
+      "time": "25-40 min",
+      "rating": "3.5",
+    },
+    {
+      "image": food1,
+      "title": "Greasy Spoon Tjarhovsgatan",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "cost": "€3.50",
+      "time": "15-20 min",
+      "rating": "4.5",
+    },
+    {
+      "image": food2,
+      "title": "Expresso House Gotgatan",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "cost": "€2.50",
+      "time": "25-40 min",
+      "rating": "3.5",
+    },
+    {
+      "image": food1,
+      "title": "Greasy Spoon Tjarhovsgatan",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "cost": "€3.50",
+      "time": "15-20 min",
+      "rating": "4.5",
+    },
+    {
+      "image": food2,
+      "title": "Expresso House Gotgatan",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "cost": "€2.50",
+      "time": "25-40 min",
+      "rating": "3.5",
+    },
+    {
+      "image": food1,
+      "title": "Greasy Spoon Tjarhovsgatan",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "cost": "€3.50",
+      "time": "15-20 min",
+      "rating": "4.5",
+    },
+    {
+      "image": food2,
+      "title": "Expresso House Gotgatan",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "cost": "€2.50",
+      "time": "25-40 min",
+      "rating": "3.5",
+    },
+  ];
+}
--- lib/view/search/search_screen.dart
+++ lib/view/search/search_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
+import 'package:sped/utils/colors.dart';
+import 'package:sped/utils/photos.dart';
+import 'package:sped/view/search/controller/search_controller.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
@@ -6,9 +11,221 @@ class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Search Screen"),
-      ),
-    );
+        //appbar with search bar and filter icon
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Container(
+            height: Get.height * 0.06,
+            padding: EdgeInsets.symmetric(
+              horizontal: Get.width * 0.02,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(Get.width * 0.3),
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                prefixIcon: const Icon(Icons.search),
+                hintText: "Search",
+                hintStyle: const TextStyle(
+                  color: Colors.grey,
+                ),
+                suffixIcon: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Icon(FontAwesomeIcons.solidCircleXmark),
+                    SizedBox(
+                      width: Get.width * 0.02,
+                    ),
+                    const Text("|"),
+                    SizedBox(
+                      width: Get.width * 0.02,
+                    ),
+                    Image.asset(
+                      filterIcon,
+                      height: Get.height * 0.03,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        body: GetBuilder<SearchesController>(
+          init: SearchesController(),
+          builder: (controller) {
+            return Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: Get.width * 0.05,
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: RichText(
+                          overflow: TextOverflow.ellipsis,
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: "Searching near ",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleMedium!
+                                    .copyWith(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                              ),
+                              TextSpan(
+                                text: "Grand hotel, Sodra Blasieholmshamnen",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleMedium!
+                                    .copyWith(
+                                      color: AppColors.logoColor,
+                                    ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: Get.width * 0.02,
+                      ),
+                      Image.asset(
+                        dropDownArrow,
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: Get.height * 0.02,
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: controller.searchList.length,
+                        itemBuilder: (context, index) => Container(
+                              margin: EdgeInsets.only(
+                                bottom: Get.height * 0.02,
+                              ),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    height: Get.height * 0.12,
+                                    width: Get.height * 0.12,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(
+                                        Get.width * 0.02,
+                                      ),
+                                      image: DecorationImage(
+                                        image: AssetImage(
+                                          controller.searchList[index]["image"],
+                                        ),
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: Get.width * 0.02,
+                                  ),
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          controller.searchList[index]["title"],
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .titleMedium!
+                                              .copyWith(
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                        ),
+                                        Text(
+                                          controller.searchList[index]
+                                              ["description"],
+                                          overflow: TextOverflow.ellipsis,
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodyText2!
+                                              .copyWith(
+                                                color: Colors.grey,
+                                              ),
+                                        ),
+                                        Row(
+                                          children: [
+                                            Icon(
+                                              Icons.delivery_dining,
+                                              color: AppColors.logoColor,
+                                            ),
+                                            SizedBox(
+                                              width: Get.width * 0.01,
+                                            ),
+                                            Text(
+                                              controller.searchList[index]
+                                                  ["cost"],
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .bodyMedium,
+                                            ),
+                                            Spacer(),
+                                            Icon(
+                                              Icons.timer,
+                                              color: AppColors.logoColor,
+                                            ),
+                                            SizedBox(
+                                              width: Get.width * 0.01,
+                                            ),
+                                            Text(
+                                              controller.searchList[index]
+                                                  ["time"],
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .bodyMedium,
+                                            ),
+                                            Spacer(),
+                                            Icon(
+                                              Icons.star,
+                                              color: AppColors.logoColor,
+                                            ),
+                                            SizedBox(
+                                              width: Get.width * 0.01,
+                                            ),
+                                            Text(
+                                              controller.searchList[index]
+                                                  ["rating"],
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .bodyMedium,
+                                            ),
+                                          ],
+                                        ),
+                                        Row(
+                                          children: [
+                                            Image.asset(
+                                              discountIcon,
+                                              color: AppColors.logoColor,
+                                            ),
+                                            SizedBox(
+                                              width: Get.width * 0.01,
+                                            ),
+                                            Text("Free Delivery"),
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )),
+                  )
+                ],
+              ),
+            );
+          },
+        ));
   }
 }
